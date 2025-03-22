94ec3f544a7669a0ca32a170a8e4d043abd0b8ee
PuneethM612 <puneethm3627@gmail.com>
2025-03-22 11:31:59 +0530

    Add missing getErrorPath() method to CustomErrorController

--- src/main/java/com/example/marksmanagement/controller/CustomErrorController.java
+++ src/main/java/com/example/marksmanagement/controller/CustomErrorController.java
@@ -39,4 +39,9 @@ public class CustomErrorController implements ErrorController {
         
         return "error";
     }
+    
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 } 
